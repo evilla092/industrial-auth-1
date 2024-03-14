7ade280606524b9359c3f302208355117307389f
Eduardo Villa <151227243+evilla092@users.noreply.github.com>
2024-03-14 20:15:15 +0000

    Fixed unfollow

--- app/policies/follow_request_policy.rb
+++ app/policies/follow_request_policy.rb
@@ -1,5 +1,12 @@
 class FollowRequestPolicy < ApplicationPolicy
 
+  attr_reader :user, :follow_request
+
+  def initialize(user, follow_request)
+    @user = user
+    @follow_request = follow_request
+  end
+
   def new?
     true
   end
